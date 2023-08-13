bc8e55ce9616ea656065942c6a44cfac9d120b89
Cyril John De Leon <deleo.cyriljohn21@gmail.com>
2023-08-13 14:38:04 +0800

    'changing design add information in about'

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,10 +3,10 @@ import "./home.css";
 import mySelf from "/pictures/me.png";
 import portfolioProject from "/pictures/firstPortfoilio.png";
 import bmiCalculator from "/pictures/bmi.png";
-import jsGame from '/pictures/jsgame.png';
-import galana from '/pictures/galana.png';
-import logo from '/pictures/logo.png'
-import wanderList from '/pictures/wanderlist.png'
+import jsGame from "/pictures/jsgame.png";
+import galana from "/pictures/galana.png";
+import logo from "/pictures/logo.png";
+import wanderList from "/pictures/wanderlist.png";
 import {
   FaCss3,
   FaHtml5,
@@ -23,20 +23,20 @@ import {
 import { useState } from "react";
 
 function Home() {
-
   const [toggle, setToggle] = useState(false);
 
-  function handleKey () {
-      if(true) {
-          setToggle(!toggle)
-      }
+  function handleKey() {
+    if (true) {
+      setToggle(!toggle);
+    }
   }
-  
 
   return (
     <div>
       <header>
-        <p><img src={logo} alt="cjlogo" id='logo'/></p>
+        <p>
+          <img src={logo} alt="cjlogo" id="logo" />
+        </p>
         <nav>
           <ul>
             <li>
@@ -52,19 +52,18 @@ function Home() {
           </ul>
         </nav>
       </header>
-      <div id="hamburger" onClick={(handleKey)}>
-            <div id="burgerLine"></div>
-            <div id="burgerLine"></div>
-            <div id="burgerLine"></div>
-            {
-                toggle?<div id="show">
-<a href="#home">Home</a>
-<a href="#about">About</a>
-<a href="#works">Works</a>
-                </div>:null
-            }
-      
-            </div>
+      <div id="hamburger" onClick={handleKey}>
+        <div id="burgerLine"></div>
+        <div id="burgerLine"></div>
+        <div id="burgerLine"></div>
+        {toggle ? (
+          <div id="show">
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+            <a href="#works">Works</a>
+          </div>
+        ) : null}
+      </div>
       {/* FOR HOME section*/}
       <section id="home">
         <h1 id="title">Fullstack Web Developer</h1>
@@ -74,7 +73,6 @@ function Home() {
         </p>
 
         <a href="/pdf/resume.pdf">
-    
           <button id="resume">Download RESUME</button>
         </a>
       </section>
@@ -86,30 +84,55 @@ function Home() {
           <img src={mySelf} alt="My Self" id="mySelf" />
           <div id="abouText">
             <p>
-            
-              I'm Cyril John Deleon,<br />a Fullstack Web Developer, eager to learn and contribute to web projects with my growing skills in both front-end and back-end development.
+              Cyril John De Leon: Exploring the Web and Rocking to New
+              Adventures! <br /> <br />
+              I'm diving into web development, learning to build awesome
+              websites. Besides coding, I'm a big fan of rock music. Just like a
+              song has different parts, coding brings elements together. I got
+              into web development because I wanted to make cool stuff on the
+              internet. Websites are like digital playgrounds. It's amazing how
+              they work, and I want to be a part of creating them. So, here I
+              am, taking my first steps in web development, excited to learn and
+              contribute to the online world. Thanks for joining me on this
+              journey!
             </p>
             <div>
               <h3>CERTIFICATE</h3>
               <p>
-               <strong>MAY 2015</strong>   <br />
-                 Computer Literacy Program (Module II Computer
-                Hardware Software and Graphic Design) Datacom Institute of
-                Computer Technology (Rosario Branch)
+                MAY 2015 <br />
+                Computer Literacy Program (Module II Computer Hardware Software
+                and Graphic Design) Datacom Institute of Computer Technology
+                (Rosario Branch)
+                <br />
+                <br />
+                The Computer Literacy Program I attended at Datacom Institute
+                focused on Computer Hardware, Software, and Graphic Design.
+                While not directly related to web development, it could have
+                imparted foundational technology knowledge and potentially
+                enhanced my understanding of hardware-software interactions,
+                which could be valuable for troubleshooting and optimizing web
+                applications. Additionally, graphic design skills learned might
+                have offered insights into user interface design and aesthetics
+                for web projects.
+                <br />
+              </p>
+              <p>
+                <br />
+                <br />
+                Feb 20 2023 - Aug 11 2023 <br />
+                6-month Part Time FullStack Web Development Program <br />
+                <br />
+                The 6-month Part-Time FullStack Web Development Program I
+                undertook provided an in-depth education on both front-end and
+                back-end development. Through this program, I acquired a range
+                of skills including HTML, CSS, JavaScript, React.js, Node.js,
+                and MongoDB, enabling me to build dynamic and interactive web
+                applications. The training honed my ability to create
+                user-friendly interfaces, handle data processing, and manage
+                server-side functionalities, ultimately equipping me to
+                contribute effectively to fullstack development projects.
               </p>
               <br />
-            
-              <p>
-               <strong>Feb 20 2023 - Aug 11 2023 </strong> <br />
-              
-                6-month Part Time FullStack Web Development Program 
-                </p> 
-                <br />
-              <h3>Random Facts About Me </h3>
-              <p>
-                I'm a fan of Slapshock and Greyhoundz. I enjoy listening to rock
-                music and sometimes I have my own concert in the bathroom .
-              </p>
             </div>
           </div>
         </div>
@@ -119,7 +142,6 @@ function Home() {
         <h1>Skills</h1>
         <div id="mySkills">
           <div>
-           
             <p id="html">
               <FaHtml5 />
               Html
@@ -150,47 +172,73 @@ function Home() {
           </p>
         </div>
       </section>
+      <br />
+      <p id="description">
+        These skills are my toolkit for crafting applications! HTML acts as the
+        foundation, helping me structure content like arranging pieces of a
+        puzzle. CSS lets me add my creative touch, painting the visual appeal of
+        the app with colors and styles. With Node.js, I'm the backstage
+        magician, making sure everything runs smoothly behind the scenes.
+        React.js is my way of bringing life to the app, creating interactive
+        elements that users can enjoy. And MongoDB is like my memory bank,
+        storing and retrieving data for the app's functionality. All in all,
+        these skills empower me to bring my app ideas to life!
+      </p>
       {/* FOR WORKS SECTION  */}
       <section id="works">
         <h1 id="workH1">Projects</h1>
         <div id="project">
           <div>
             <h1>Portfolio Project</h1>
-            <Link to="https://htmlcss21.netlify.app/">
-              <img
-                src={portfolioProject}
-                alt="firstProject portfolio"
-                id="firstProject"
-              />
-            </Link>
+
+            <img
+              src={portfolioProject}
+              alt="firstProject portfolio"
+              id="firstProject"
+            />
+
             <p>
               Welcome to my very first portfolio project, a simple yet
               captivating demonstration of my HTML and CSS skills. This project
-              showcases my journey into web development. <br/>
-              Code Source :<Link to='https://github.com/cyyy21/p1-web-dev-portfolio'><span>Click<FaCode/></span></Link>  
+              showcases my journey into web development. <br />
+              <div>
+                <Link to="https://github.com/cyyy21/p1-web-dev-portfolio">
+                  <button id="codeSource">CODE SOURCE</button>{" "}
+                </Link>
+                <Link to="https://htmlcss21.netlify.app/">
+                  {" "}
+                  <button id="liveServer">LIVE SERVER</button>{" "}
+                </Link>
+              </div>
             </p>
           </div>
 
           <div>
             <h1>Javascript Game</h1>
-            <Link to='https://rushhour-js.netlify.app/'>
+
             <img src={jsGame} alt="jsgamepicture" />
-            </Link>
+
             <p>
               "Navigate the Rush Hour: Dodge and weave through traffic in this
               thrilling JavaScript game. Avoid oncoming cars and test your
               reflexes to reach the high score. Use KeyA to move left and KeyD
-              to move right. Can you conquer the road and stay safe?"<br />
-              Code Source : <Link to='https://github.com/cyyy21/p2-js-game'><span>Click<FaCode/></span></Link>
+              to move right. Can you conquer the road and stay safe?"
+              <br />
+              <Link to="https://github.com/cyyy21/p2-js-game">
+                <button id="codeSource">CODE SOURCE</button>{" "}
+              </Link>
+              <Link to="https://rushhour-js.netlify.app/">
+                {" "}
+                <button id="liveServer">LIVE SERVER</button>{" "}
+              </Link>
             </p>
-        
           </div>
-          
+
           <div>
             <h1>BMI CALCULATOR</h1>
-            <Link to="https://bmi-calculatorproj.netlify.app/">
-              <img src={bmiCalculator} alt="bmi project" id="bmi" />
-            </Link>
+
+            <img src={bmiCalculator} alt="bmi project" id="bmi" />
+
             <p>
               "Stay Informed with BMI Calculator: Take charge of your health
               journey effortlessly with the BMI Calculator app. By effortlessly
@@ -198,52 +246,84 @@ function Home() {
               mass index (BMI) - a vital indicator of your weight status.
               Discover whether you're within the healthy weight range and make
               informed choices for your well-being. Simplify your health
-              assessment today! <br/>
-              Code Source : <Link to='https://github.com/cyyy21/p3-react-app'><span>Click<FaCode/></span></Link>
+              assessment today! <br />
+              <Link to="https://github.com/cyyy21/p3-react-app">
+                <button id="codeSource">CODE SOURCE</button>{" "}
+              </Link>
+              <Link to="https://bmi-calculatorproj.netlify.app/">
+                {" "}
+                <button id="liveServer">LIVE SERVER</button>{" "}
+              </Link>
             </p>
           </div>
-          
+
           <div>
-          <h1>GALANA APP </h1>
-          <Link to="https://galana-app.onrender.com"> <img src={galana} alt="galana" /></Link>
-          <p>
-            The Galana App is  application designed to
-            help you add travel information. With this app, you can add, edit,
-            and delete places to blog about your hotels or places. <br />
-            Code Source FRONT-END: <Link to='https://github.com/cyyy21/p4-node-frontend'><span>CODE<FaCode/></span></Link><br />
-            Code Source BACK-END:  <Link to='https://github.com/cyyy21/p4-node-backend'><span>CODE<FaCode/></span></Link><br />
-          </p>
+            <h1>GALANA APP </h1>
+
+            <img src={galana} alt="galana" />
+
+            <p>
+              The Galana App is application designed to help you add travel
+              information. With this app, you can add, edit, and delete places
+              to blog about your hotels or places. <br />
+              <Link to="https://github.com/cyyy21/p4-node-frontend">
+                <button id="codeSource">frontEnd Code</button>{" "}
+              </Link>
+              <Link to="https://github.com/cyyy21/p4-node-backend">
+                <button id="codeSource">BackEnd Code </button>{" "}
+              </Link>
+              <Link to="https://galana-app.onrender.com">
+                {" "}
+                <button id="liveServer">LIVE SERVER</button>{" "}
+              </Link>
+            </p>
           </div>
 
-       
-        <div>
-        <h1>WanderList</h1>
-        <Link to='https://wanderlist-lchl.onrender.com/'><img src={wanderList} alt="wandeerlist- homepage" /></Link>
-        <p>
-          <span>TEAM PROJECT :</span> The Itinerary App is a user-friendly application designed to help you organize and plan your travel adventures efficiently.
-        <br />Code Source : <br />
-       <Link to='https://github.com/NikkoVP/FullStack-MERN-Team-Project'><span>Click<FaCode/></span></Link>
+          <div>
+            <h1>WanderList</h1>
 
-        </p>
-        </div>
+            <img src={wanderList} alt="wandeerlist- homepage" />
+
+            <p>
+              <span>TEAM PROJECT :</span> The Itinerary App is a user-friendly
+              application designed to help you organize and plan your travel
+              adventures efficiently.
+              <br />
+              <Link to="https://github.com/NikkoVP/FullStack-MERN-Team-Project">
+                <button id="codeSource">Code Source</button>{" "}
+              </Link>
+              <Link to="https://wanderlist-lchl.onrender.com/">
+                {" "}
+                <button id="liveServer">LIVE SERVER</button>{" "}
+              </Link>
+            </p>
+          </div>
         </div>
         <div id="line"></div>
       </section>
-      
+
       <footer id="accounts">
-      <div id="accountsContainer" >
-        <h1>Accounts</h1>
-        <div id="socialMedia" >
-        <Link to='https://github.com/cyyy21' id="github"><FaGithub/></Link>
-       <Link to='https://www.linkedin.com/in/cyril-john-de-leon-2bbb08273/' id="linkedIn"><FaLinkedin/></Link>
-        <Link to='https://www.messenger.com/t/100004758971517' id="messenger"><FaFacebookMessenger/></Link>
-        <Link to='mailto:deleon.cyriljohn21@gmail.com' id='gmail'><FaGoogle/></Link>
-     
-           </div>
-       
+        <div id="accountsContainer">
+          <h1>Accounts</h1>
+          <div id="socialMedia">
+            <Link to="https://github.com/cyyy21" id="github">
+              <FaGithub />
+            </Link>
+            <Link
+              to="https://www.linkedin.com/in/cyril-john-de-leon-2bbb08273/"
+              id="linkedIn">
+              <FaLinkedin />
+            </Link>
+            <Link
+              to="https://www.messenger.com/t/100004758971517"
+              id="messenger">
+              <FaFacebookMessenger />
+            </Link>
+            <Link to="mailto:deleon.cyriljohn21@gmail.com" id="gmail">
+              <FaGoogle />
+            </Link>
+          </div>
         </div>
-    
-  
       </footer>
     </div>
   );
